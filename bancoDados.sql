c3b18ecad8bec4a2c24a40dac08fe4ac31ed9f64
Theodoro Bertol <dev.theodoro@gmail.com>
2023-12-14 20:14:53 -0300

    socorro

--- bancoDados.sql
+++ bancoDados.sql
@@ -6,7 +6,6 @@ USE SuinoCulturaTheo;
 CREATE TABLE IF NOT EXISTS entradaracao (
     id INT AUTO_INCREMENT PRIMARY KEY,
     quantidadeRacao INT,
-    validadeRacao DATE,
     data DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
